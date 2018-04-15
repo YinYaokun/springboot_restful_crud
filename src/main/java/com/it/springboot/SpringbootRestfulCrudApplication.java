9185ab0a717d92311e01257779b7e26f406ddcc3
YinYaokun <2694941044@qq.com>
2018-04-15 22:34:36 +0800

    Initial commit

---
+++ src/main/java/com/it/springboot/SpringbootRestfulCrudApplication.java
@@ -0,0 +1,12 @@
+package com.it.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRestfulCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRestfulCrudApplication.class, args);
+	}
+}
